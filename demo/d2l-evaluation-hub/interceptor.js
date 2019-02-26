2d2f570204622cc5aba08663fcf55ddf759a0fa9
Alex Bedley <alex.bedley@D2L.com>
2019-02-26 15:27:19 -0500

    Add debugging to the interceptor and turn it on

--- demo/d2l-evaluation-hub/interceptor.js
+++ demo/d2l-evaluation-hub/interceptor.js
@@ -1,14 +1,33 @@
-function startMockServer(mappings) {
+function startMockServer(mappings, debug) {
 	const oldFetch = window.d2lfetch.fetch.bind(window.d2lfetch);
+
+	if (debug) {
+		console.group('All Mappings');
+		console.log(mappings);
+		console.groupEnd();
+	}
+
 	window.d2lfetch.fetch = function(input, options) {
-		return intercept(input, options, mappings).catch(() => {
+		if (debug) {
+			console.groupCollapsed(input);
+		}
+		return intercept(input, options, mappings, debug).catch(() => {
+			if (debug) {
+				console.log('No mappings found, falling back to d2lfetch.fetch');
+				console.groupEnd();
+			}
 			return oldFetch(input, options);
 		});
 	};
 }
 
-function intercept(input, options, mappings) {
+function intercept(input, options, mappings, debug) {
 	if (mappings[input]) {
+		if (debug) {
+			console.dir(mappings[input]);
+			console.log(JSON.stringify(mappings[input], null, 2));
+			console.groupEnd();
+		}
 		return Promise.resolve({
 			ok: true,
 			json: function() {
