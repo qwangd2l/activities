3be07a3fb6c66caa092ebad35c72229599909c0e
Josh Roach <josh.roach@D2L.com>
2019-03-14 09:24:45 -0400

    Add title and improve code formatting

--- components/d2l-evaluation-hub/d2l-evaluation-hub-activities-list.js
+++ components/d2l-evaluation-hub/d2l-evaluation-hub-activities-list.js
@@ -145,7 +145,11 @@ class D2LEvaluationHubActivitiesList extends mixinBehaviors([D2L.PolymerBehavior
 							<d2l-tr>
 								<d2l-td class="d2l-username-column">
 									<d2l-offscreen id="d2l-evaluation-hub-activities-list-username">[[localize('evaluate')]][[_getDataProperty(s, 'displayName')]]</d2l-offscreen>
-									<d2l-link aria-describedby$="d2l-evaluation-hub-activities-list-username" href="[[s.activityLink]]">[[_getDataProperty(s, 'displayName')]]</d2l-link>
+									<d2l-link
+										title="[[localize('evaluate')]][[_getDataProperty(s, 'displayName')]]"
+										aria-describedby$="d2l-evaluation-hub-activities-list-username"
+										href="[[s.activityLink]]">[[_getDataProperty(s, 'displayName')]]
+									</d2l-link>
 									<d2l-activity-evaluation-icon-base draft$="[[s.isDraft]]"></d2l-activity-evaluation-icon-base>
 								</d2l-td>
 								<d2l-td class="d2l-evaluation-hub-truncated-column d2l-activity-name-column">
