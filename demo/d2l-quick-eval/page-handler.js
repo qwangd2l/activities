45f47c6386fdb22a0e8f54b5c732bedab46fdaab
Miles Tjandrawidjaja <miles.tjandrawidjaja@d2l.com>
2019-03-19 15:11:18 -0400

    US104726 - Handle bookmarking in demo data

--- demo/d2l-quick-eval/page-handler.js
+++ demo/d2l-quick-eval/page-handler.js
@@ -2,6 +2,29 @@ import { parseSortFromUrl, encodeSortState, decodeSortState } from './sort-handl
 import { formatActivities } from './activity-handler';
 import chunk from 'lodash-es/chunk';
 
+function getQueryParamOrDefault(
+	relativeUrl,
+	queryParam,
+	defaultValue
+) {
+	const baseUrl = 'http://www.test.com/'
+	const fullUrl = new URL(baseUrl + relativeUrl);
+
+	return fullUrl.searchParams.get(queryParam) || defaultValue;
+}
+
+function setQueryParam(
+	relativeUrl,
+	queryParam,
+	value
+) {
+	const baseUrl = 'http://www.test.com/'
+	const fullUrl = new URL(baseUrl + relativeUrl);
+	fullUrl.searchParams.set(queryParam, value);
+
+	return fullUrl.pathname.substr(1) + '?' + fullUrl.searchParams.toString();
+}
+
 function applySorts(activities, sorts, sortState) {
 	sortState.reverse().forEach(appliedSort => {
 		const sortDefn = sorts.filter(s => s.id === appliedSort.id)[0];
@@ -31,23 +54,35 @@ function createPageEndpoint(activities, sorts, pageSize, pageNumber, filtersHref
 	var shouldFail = failFirstTime;
 
 	return (url) => {
+
+		const bookmark = parseInt( getQueryParamOrDefault(url, 'bookmark', 0) );
+		const nextBookmark = bookmark + parseInt( pageSize ) ;
+
 		const serializedSortState = parseSortFromUrl(url);
 		const sortState = decodeSortState(serializedSortState);
 
 		const sortedActivities = applySorts(activities, sorts, sortState);
 		const formattedSortedActivities = formatActivities(sortedActivities);
-		const pagedActivities = chunk(formattedSortedActivities, pageSize);
+		const pagedActivities = formattedSortedActivities.slice(bookmark, Math.min(formattedSortedActivities.length, nextBookmark));
+
+		const nextPageHrefWithBookmark = setQueryParam(url, 'bookmark', nextBookmark);
+		const nextPageHrefWithBookmarkAndSort = setQueryParam(nextPageHrefWithBookmark, 'sort', encodeSortState(sortState));
+
+		const nextSortHrefWithBookmark = setQueryParam(sortsHref, 'bookmark', nextBookmark);
+		const nextSortHrefWithBookmarkAndSort = setQueryParam(nextSortHrefWithBookmark, 'sort', encodeSortState(sortState));
 
 		if (shouldFail) {
 			shouldFail = false;
 			throw new Error('simulated error');
 		}
+		const hasMoreActivities = nextBookmark < activities.length;
 
-		return formatPage(pagedActivities[pageNumber], filtersHref, addSortToHref(sortsHref, sortState), addSortToHref(nextPageHref, sortState));
+
+		return formatPage(pagedActivities, filtersHref, nextSortHrefWithBookmarkAndSort, nextPageHrefWithBookmarkAndSort, hasMoreActivities);
 	};
 }
 
-function formatPage(entities, filterLocation, sortsLocation, nextLocation) {
+function formatPage(entities, filterLocation, sortsLocation, nextLocation, hasMoreActivities) {
 
 	const entity = {
 		'links': [
@@ -68,7 +103,7 @@ function formatPage(entities, filterLocation, sortsLocation, nextLocation) {
 		'entities': entities
 	};
 
-	if (nextLocation) {
+	if (hasMoreActivities) {
 		entity.links.push({
 			'rel': [
 				'next'
