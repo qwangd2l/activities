9d4c200f92a59f3c9cb43029c8aa4b7ea5acb469
pork <vix633@gmail.com>
2019-03-22 16:10:04 -0400

    minify svg

--- components/d2l-quick-eval/d2l-quick-eval-skeleton-no-headers.js
+++ components/d2l-quick-eval/d2l-quick-eval-skeleton-no-headers.js
@@ -26,55 +26,43 @@ class D2LEvaluationHubSkeletonNoHeaders extends PolymerElement {
                     }
                 }
             </style>
-             <div class="desktop">
-                <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-                <svg width="100%" viewBox="0 0 1170 197" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <!-- Generator: Sketch 50.2 (55047) - http://www.bohemiancoding.com/sketch -->
-                    <title>EH - Thin - Flat List - Submission - oldest to newest</title>
-                    <desc>Created with Sketch.</desc>
-                    <defs></defs>
-                    <g id="EH---Thin---Flat-List---Submission---oldest-to-newest" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <rect id="Rectangle" fill="#f2f3f5" x="0" y="38" width="30" height="30" rx="4"></rect>
-                        <rect id="Rectangle-Copy-9" fill="#f2f3f5" x="0" y="93" width="30" height="30" rx="4"></rect>
-                        <rect id="Rectangle-Copy-15" fill="#f2f3f5" x="0" y="148" width="30" height="30" rx="4"></rect>
-                        <rect id="Rectangle-Copy-6" fill="#f2f3f5" x="280" y="45" width="18" height="18" rx="4"></rect>
-                        <rect id="Rectangle-Copy-10" fill="#f2f3f5" x="280" y="100" width="18" height="18" rx="4"></rect>
-                        <rect id="Rectangle-Copy-16" fill="#f2f3f5" x="280" y="155" width="18" height="18" rx="4"></rect>
-                        <rect id="Rectangle" fill="#f2f3f5" x="40" y="46" width="120" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-11" fill="#f2f3f5" x="40" y="101" width="120" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-17" fill="#f2f3f5" x="40" y="156" width="120" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-5" fill="#f2f3f5" x="315" y="46" width="240" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-12" fill="#f2f3f5" x="315" y="101" width="240" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-18" fill="#f2f3f5" x="315" y="156" width="240" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-7" fill="#f2f3f5" x="620" y="46" width="240" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-13" fill="#f2f3f5" x="620" y="101" width="240" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-19" fill="#f2f3f5" x="620" y="156" width="240" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-8" fill="#f2f3f5" x="930" y="46" width="180" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-14" fill="#f2f3f5" x="930" y="101" width="180" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-20" fill="#f2f3f5" x="930" y="156" width="180" height="14" rx="4"></rect>
-                        <path d="M 0 80 L 1170 80" id="Line-Copy" stroke="#f2f3f5" stroke-linecap="square"></path>
-                        <path d="M 0 135 L 1170 135" id="Line-Copy-2" stroke="#f2f3f5" stroke-linecap="square"></path>
+            <div class="desktop">
+                <svg viewBox="0 0 1170 197" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fill-rule="evenodd">
+                        <rect fill="#f2f3f5" y="38" width="30" height="30" rx="4"/>
+                        <rect fill="#f2f3f5" y="93" width="30" height="30" rx="4"/>
+                        <rect fill="#f2f3f5" y="148" width="30" height="30" rx="4"/>
+                        <rect fill="#f2f3f5" x="280" y="45" width="18" height="18" rx="4"/>
+                        <rect fill="#f2f3f5" x="280" y="100" width="18" height="18" rx="4"/>
+                        <rect fill="#f2f3f5" x="280" y="155" width="18" height="18" rx="4"/>
+                        <rect fill="#f2f3f5" x="40" y="46" width="120" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="40" y="101" width="120" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="40" y="156" width="120" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="315" y="46" width="240" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="315" y="101" width="240" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="315" y="156" width="240" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="620" y="46" width="240" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="620" y="101" width="240" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="620" y="156" width="240" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="930" y="46" width="180" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="930" y="101" width="180" height="14" rx="4"/>
+                        <rect fill="#f2f3f5" x="930" y="156" width="180" height="14" rx="4"/>
+                        <path d="M0 80h1170M0 135h1170" stroke="#f2f3f5" stroke-linecap="square"/>
                     </g>
-                </svg> 
+                </svg>
             </div>
             <div class="mobile">
-                <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-                <svg width="100%" viewBox="0 0 237 209" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <!-- Generator: Sketch 50.2 (55047) - http://www.bohemiancoding.com/sketch -->
-                    <title>mobile assignment end copy</title>
-                    <desc>Created with Sketch.</desc>
-                    <defs></defs>
-                    <g id="mobile-assignment-end-copy" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <rect id="Rectangle-Copy-22" fill="#F2F3F5" x="0" y="42" width="30" height="30" rx="4"></rect>
-                        <rect id="Rectangle-Copy-23" fill="#F2F3F5" x="0" y="97" width="30" height="30" rx="4"></rect>
-                        <rect id="Rectangle-Copy-24" fill="#F2F3F5" x="0" y="152" width="30" height="30" rx="4"></rect>
-                        <rect id="Rectangle-Copy-25" fill="#F2F3F5" x="39" y="50" width="120" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-26" fill="#F2F3F5" x="39" y="105" width="120" height="14" rx="4"></rect>
-                        <rect id="Rectangle-Copy-27" fill="#F2F3F5" x="39" y="160" width="120" height="14" rx="4"></rect>
-                        <path d="M-1,83.5 L237,83.5" id="Line-Copy-4" stroke="#F2F3F5" stroke-linecap="square"></path>
-                        <path d="M-1,138.5 L237,138.5" id="Line-Copy-5" stroke="#F2F3F5" stroke-linecap="square"></path>
+                <svg viewBox="0 0 237 209" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fill-rule="evenodd">
+                        <rect fill="#F2F3F5" y="42" width="30" height="30" rx="4"/>
+                        <rect fill="#F2F3F5" y="97" width="30" height="30" rx="4"/>
+                        <rect fill="#F2F3F5" y="152" width="30" height="30" rx="4"/>
+                        <rect fill="#F2F3F5" x="39" y="50" width="120" height="14" rx="4"/>
+                        <rect fill="#F2F3F5" x="39" y="105" width="120" height="14" rx="4"/>
+                        <rect fill="#F2F3F5" x="39" y="160" width="120" height="14" rx="4"/>
+                        <path d="M-1 83.5h238M-1 138.5h238" stroke="#F2F3F5" stroke-linecap="square"/>
                     </g>
-                </svg>   
+                </svg>
             </div>    
 		`;
 	}
