72217b151788006ed813addbc96b6023ba91317b
Miles Tjandrawidjaja <miles.tjandrawidjaja@d2l.com>
2019-04-16 12:59:44 -0400

    DE33386 - Add comment to file

--- components/d2l-quick-eval/compatability/universal-methods.js
+++ components/d2l-quick-eval/compatability/universal-methods.js
@@ -1,3 +1,8 @@
+/*
+All functions in this file should work across all browsers.
+Initially intended for enabling compatability with IE11, but applies to any browser missing some functionality.
+*/
+
 const StringEndsWith = (str, search) => {
 	if (typeof str !== 'string' || typeof search !== 'string') {
 		return false;
