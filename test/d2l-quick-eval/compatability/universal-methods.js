c85abc7efc724ad844b4de44df7235d7f640f45c
Miles Tjandrawidjaja <miles.tjandrawidjaja@d2l.com>
2019-04-16 12:30:50 -0400

    DE33386 - Allow QE to load in IE11 with real LMS hookup

--- test/d2l-quick-eval/compatability/universal-methods.js
+++ test/d2l-quick-eval/compatability/universal-methods.js
@@ -1,4 +1,4 @@
-import { StringEndsWith } from '../../../components/d2l-quick-eval/compatability/universal-methods.js';
+import { StringEndsWith, GetQueryStringParams, GetQueryStringParam } from '../../../components/d2l-quick-eval/compatability/universal-methods.js';
 
 suite('StringEndsWith', () => {
 	const testCases = [
@@ -24,6 +24,55 @@ suite('StringEndsWith', () => {
 
 		test(`String ${str} should return ${expectedResult} when checking for ${endingStr}`, () => {
 			assert.equal(expectedResult, StringEndsWith(str, endingStr));
+		});
+	});
+});
+
+suite('GetQueryStringParams', () => {
+	const testCases = [
+		{ queryString: undefined, expectedResult: {} },
+		{ queryString: null, expectedResult: {} },
+		{ queryString: '', expectedResult: {} },
+		{ queryString: '?x', expectedResult: {} },
+		{ queryString: '?x=', expectedResult: { x: '' } },
+		{ queryString: '?x=1&x=2', expectedResult: {x: '1'} },
+		{ queryString: '?x=1&y=2', expectedResult: {x: '1', y: '2'} },
+		{ queryString: '?x=1&&y=2', expectedResult: {x: '1', y: '2'} },
+		{ queryString: '?x=%3Fencoded%3Dstring%25%26here&y=2', expectedResult: {x: '?encoded=string%&here', y: '2'} },
+		{ queryString: '?%3F%3F=1', expectedResult: {'??': '1'} }
+	];
+
+	testCases.forEach(function(testCase) {
+		const queryString = testCase.queryString;
+		const expectedResult = testCase.expectedResult;
+
+		test(`Given ${queryString} params should be ${JSON.stringify(expectedResult)}`, () => {
+			assert.deepEqual(expectedResult, GetQueryStringParams(queryString));
+		});
+	});
+});
+
+suite('GetQueryStringParam', () => {
+	const testCases = [
+		{ url: 'https://www.d2l.com?', name: 'x', expectedResult: null },
+		{ url: 'https://www.d2l.com?x', name: 'x', expectedResult: null },
+		{ url: 'https://www.d2l.com?x=', name: 'x', expectedResult: '' },
+		{ url: 'https://www.d2l.com?x=1', name: 'y', expectedResult: null },
+		{ url: 'https://www.d2l.com?x=1', name: 'x', expectedResult: '1' },
+		{ url: 'https://www.d2l.com/?x=1', name: 'x', expectedResult: '1' },
+		{ url: 'https://www.d2l.com/?x=1&y=2', name: 'x', expectedResult: '1' },
+		{ url: 'https://www.d2l.com/?x=1&y=2', name: 'y', expectedResult: '2' },
+		{ url: 'https://www.d2l.com/?x=%3Fencoded%3Dstring%25%26here&y=2', name: 'x', expectedResult: '?encoded=string%&here' },
+		{ url: 'https://www.d2l.com/?%3F%3F=1', name: '??', expectedResult: '1' }
+	];
+
+	testCases.forEach(function(testCase) {
+		const url = testCase.url;
+		const name = testCase.name;
+		const expectedResult = testCase.expectedResult;
+
+		test(`Given ${url} searching for query param ${name} should return ${expectedResult}`, () => {
+			assert.equal(expectedResult, GetQueryStringParam(name, new window.URL(url)));
 		});
 	});
 });
